cbb32c33b8c13e7e331f448c2df48e1eb8477c1f
Peter <52173034+Ainzll@users.noreply.github.com>
2021-07-01 11:57:55 -0700

    Add files via upload

---
+++ Initials.cpp
@@ -0,0 +1,14 @@
+// g++ initials.cpp -o pv.o
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "P P P   V       V\n";
+	cout << "P   P   V       V\n";
+	cout << "P P P    V     V\n";
+	cout << "P         V   V\n";
+	cout << "P          V V\n";
+	cout << "P           V\n";
+	
+	return 0;
+}
